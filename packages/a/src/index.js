f1a652ba40f7990f443ee97f0064c365b1ab319c
Orie Steele <orie@transmute.industries>
2022-02-28 14:09:22 -0600

    chore(ci): testing :rocket:

---
+++ packages/a/src/index.js
@@ -0,0 +1,6 @@
+const add = (a, b) => {
+  console.log("hey");
+  return a + b;
+};
+
+module.exports = { add };
---
+++ packages/a/src/tests/add.test.js
@@ -0,0 +1,5 @@
+const { add } = require("../index");
+
+it("has a test", () => {
+  expect(add(1, 2)).toBe(3);
+});
