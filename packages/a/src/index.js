6d16c2cace846dde8304c7bd18caaf1eccf8f4e6
Orie Steele <orie@transmute.industries>
2022-03-01 11:21:55 -0600

    chore: add b

--- packages/a/src/index.js
+++ packages/a/src/index.js
@@ -1,7 +1,9 @@
+const { sum } = require('b');
+
 const add = (a, b) => {
   // eslint-disable-next-line no-console
   console.log('hey');
-  return a + b;
+  return sum(a, b);
 };
 
 module.exports = { add };
